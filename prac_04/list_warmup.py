e1527a5ba0fd72eec323a8638d5d0054ea5bfbba
cbowler06 <cameron.bowler@my.jcu.edu.au>
2019-08-28 12:26:57 +1000

    Finish exercise

---
+++ prac_04/list_warmup.py
@@ -0,0 +1,11 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+numbers[0] = 10
+numbers[-1] = 1
+print(numbers[2:])
+if 9 in numbers:
+    print("nine is in numbers")
+else:
+    print("nine is not in numbers")
+
+print(numbers)
