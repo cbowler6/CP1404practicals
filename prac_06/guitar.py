9a85016434217340ad16a48e4ed98fc4e7be9828
cbowler06 <cameron.bowler@my.jcu.edu.au>
2019-09-11 02:41:45 +1000

    finish all do from scratch exercises

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,17 +1,21 @@
 class Guitar:
     def __init__(self, name="", year=0, cost=0):
         self.name = name
-        self.year = year
-        self.cost = cost
+        self.year = int(year)
+        self.cost = int(cost)
 
     def __str__(self):
         return "{} ({}) : ${}".format(self.name, self.year, self.cost)
 
     def get_age(self):
-        return 2019 - self.year
+        return 2019 - int(self.year)
 
     def is_vintage(self):
         if self.get_age() > 50:
             return True
         else:
             return False
+
+    def __lt__(self, other):
+        """Less than, used for sorting Guitars - by year released."""
+        return self.year < other.year
