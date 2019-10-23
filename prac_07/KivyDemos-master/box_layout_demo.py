3e0549c524436051e506a35f90eda057acc317ea
cbowler06 <cameron.bowler@my.jcu.edu.au>
2019-10-23 20:29:01 +1000

    fixed exercises

--- prac_07/KivyDemos-master/box_layout_demo.py
+++ prac_07/KivyDemos-master/box_layout_demo.py
@@ -9,8 +9,10 @@ class BoxLayoutDemo(App):
         return self.root
 
     def handle_greet(self):
-        print("test")
         self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
+
+    def clear(self):
+        self.root.ids.input_name.text = ""
 
 
 BoxLayoutDemo().run()
